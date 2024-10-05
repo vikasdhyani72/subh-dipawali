589b0fa9baa9aee788e885e5b0d2f787761c61d8
Vikas Dhyani <vikasdhyani72@gmail.com>
2024-10-05 13:35:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const receiverName = document.querySelector('#receiver')
+const senderName = document.querySelector('#sender')
+receiverName.innerText = new URLSearchParams(location.search).get('receiver')
+senderName.innerText =
+  new URLSearchParams(location.search).get('sender') || 'Vikas Dhyani'
